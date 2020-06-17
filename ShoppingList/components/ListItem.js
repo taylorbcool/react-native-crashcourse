0bfb7530b9729e910a9f4650acc145542799fdd0
Taylor Cool <taycool@gmail.com>
2020-06-17 16:28:21 -0700

    finished shopping list

--- ShoppingList/components/ListItem.js
+++ ShoppingList/components/ListItem.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const ListItem = props => {
+const ListItem = ({
+  item,
+  deleteItem,
+  editItem,
+  isEditing,
+  editItemDetail,
+  saveEditItem,
+  handleEditChange,
+  itemChecked,
+  checkedItems,
+}) => {
+  const checked = checkedItems.filter(
+    checkedItem => checkedItem.id === item.id,
+  );
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-        <Text onPress={() => props.deleteItem(item.id)} style={styles.listItemText}>{props.item.text}</Text>
-        {/* <Icon name="remove" size={20} color='firebrick' onPress={() => props.deleteItem(item.id)} /> */}
+        {isEditing && editItemDetail.id === item.id ? (
+          <TextInput
+            placeholder="Edit Item..."
+            style={styles.editItemInput}
+            onChangeText={handleEditChange}
+          />
+        ) : (
+          <Text
+            onPress={() => itemChecked(item.id, item.text)}
+            style={
+              checked.length ? styles.checkedItemText : styles.listItemText
+            }>
+            {item.text}
+          </Text>
+        )}
+        <View style={styles.iconView}>
+          {isEditing && editItemDetail.id === item.id ? (
+            <Icon
+              name="save"
+              size={20}
+              color="green"
+              onPress={() => saveEditItem(item.id, item.text)}
+            />
+          ) : (
+            !checked.length && (
+              <Icon
+                name="pencil"
+                size={20}
+                color="blue"
+                onPress={() => editItem(item.id, item.text)}
+              />
+            )
+          )}
+          <Icon
+            name="remove"
+            size={20}
+            color="firebrick"
+            onPress={() => deleteItem(item.id)}
+          />
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -17,7 +75,7 @@ const styles = StyleSheet.create({
     padding: 15,
     backgroundColor: '#f8f8f8',
     borderBottomWidth: 1,
-    borderColor: '#eee'
+    borderColor: '#eee',
   },
   listItemView: {
     flexDirection: 'row',
@@ -26,7 +84,21 @@ const styles = StyleSheet.create({
   },
   listItemText: {
     fontSize: 18,
-  }
+  },
+  checkedItemText: {
+    fontSize: 18,
+    textDecorationLine: 'line-through',
+    color: 'green',
+  },
+  iconView: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 70,
+  },
+  editItemInput: {
+    padding: 0,
+    fontSize: 18,
+  },
 });
 
 export default ListItem;
